2276ea9a7a91d130bc54e764ebb2d947a16901cc
bimashazaman <bimashazaman1234@gmail.com>
2022-07-04 03:57:39 +0600

    added blade files

--- resources/views/partials/RequestTour.blade.php
+++ resources/views/partials/RequestTour.blade.php
@@ -60,63 +60,67 @@
             </ul> --}}
             </div>
             <div class="row">
-                <div class="col-md-6">
-                    <img src="{{ asset('images/ReqTour1.png') }}" alt="">
+<div class="col-md-12 col-xs-12">
+    <div class="col-md-6 col-xs-6">
+        <img src="{{ asset('images/ReqTour1.png') }}" alt="">
+    </div>
+    <div class="col-md-6 col-xs-6">
+
+        <form id="multistepsform">
+            <!-- progressbar -->
+
+            <!-- fieldsets -->
+            <fieldset>
+
+
+                <input type="text" placeholder="Name">
+                <input type="Text" placeholder="Start Date">
+                <input type="text" list="cars" placeholder="Destination" />
+                <datalist id="Starting Destination">
+                    <option>Volvo</option>
+                    <option>Saab</option>
+                    <option>Mercedes</option>
+                    <option>Audi</option>
+                </datalist>
+                <input type="number" placeholder="Number of people">
+                <div class="parentForm">
+                    <div class="divForm1"><i class="fa-solid fa-car"></i> Car </div>
+                    <div class="divForm2"> <i class="fa-solid fa-bicycle"></i> Cycle</div>
+                    <div class="divForm3"> <i class="fa-solid fa-person-hiking"></i> Hiking</div>
+                    <div class="divForm4"><i class="fa-solid fa-person-biking"></i> Bike </div>
+                    <div class="divForm5"><i class="fa-solid fa-bus"></i>Bus </div>
+                    <div class="divForm6"><i class="fa-solid fa-car-bus"></i>Car Bus </div>
                 </div>
-                <div class="col-md-6">
-
-                    <form id="multistepsform">
-                        <!-- progressbar -->
-
-                        <!-- fieldsets -->
-                        <fieldset>
-
-
-                            <input type="text" placeholder="Name">
-                            <input type="Text" placeholder="Start Date">
-                            <input type="text" list="cars" placeholder="Destination" />
-                            <datalist id="Starting Destination">
-                                <option>Volvo</option>
-                                <option>Saab</option>
-                                <option>Mercedes</option>
-                                <option>Audi</option>
-                            </datalist>
-                            <input type="number" placeholder="Number of people">
-                            <div class="parentForm">
-                                <div class="divForm1"><i class="fa-solid fa-car"></i> Car </div>
-                                <div class="divForm2"> <i class="fa-solid fa-bicycle"></i> Cycle</div>
-                                <div class="divForm3"> <i class="fa-solid fa-person-hiking"></i> Hiking</div>
-                                <div class="divForm4"><i class="fa-solid fa-person-biking"></i> Bike </div>
-                                <div class="divForm5"><i class="fa-solid fa-bus"></i>Bus </div>
-                                <div class="divForm6"><i class="fa-solid fa-car-bus"></i>Car Bus </div>
-                            </div>
-                            <input type="button" name="next" class="package-view" style="width: 80px;box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;" value="Next" />
+                <input type="button" name="next" class="package-view" style="width: 80px;box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;" value="Next" />
 
 
 
-                        </fieldset>
+            </fieldset>
 
-                        <fieldset>
-                            <input type="text" name="fname" placeholder="First Name" />
-                            <input type="text" name="lname" placeholder="Last Name" />
-                            <input type="text" name="phone" placeholder="Phone" />
-                            <textarea name="address" placeholder="Address"></textarea>
-                            <input type="button" name="previous" class="previous action-button" value="Previous" />
+            <fieldset>
+                <input type="text" name="fname" placeholder="First Name" />
+                <input type="text" name="lname" placeholder="Last Name" />
+                <input type="text" name="phone" placeholder="Phone" />
+                <textarea name="address" placeholder="Address"></textarea>
+                <input type="button" name="previous" class="previous action-button" value="Previous" />
 
-                            <input type="button" name="next" class="next action-button" value="Next" />
-                        </fieldset>
-                        <fieldset>
-                            <h2 class="fs-title">Social Profiles</h2>
-                            <h3 class="fs-subtitle">Your presence on the social network</h3>
-                            <input type="text" name="twitter" placeholder="Twitter" />
-                            <input type="text" name="facebook" placeholder="Facebook" />
-                            <input type="text" name="gplus" placeholder="Google Plus" />
-                            <input type="button" name="previous" class="previous action-button" value="Previous" />
-                            <input type="submit" name="submit" class="submit action-button" value="Submit" />
-                        </fieldset>
-                    </form>
+                <input type="button" name="next" class="next action-button" value="Next" />
+            </fieldset>
+            <fieldset>
+                <h2 class="fs-title">Social Profiles</h2>
+                <h3 class="fs-subtitle">Your presence on the social network</h3>
+                <input type="text" name="twitter" placeholder="Twitter" />
+                <input type="text" name="facebook" placeholder="Facebook" />
+                <input type="text" name="gplus" placeholder="Google Plus" />
+                <input type="button" name="previous" class="previous action-button" value="Previous" />
+                <input type="submit" name="submit" class="submit action-button" value="Submit" />
+            </fieldset>
+        </form>
 
-                </div>
+
+   </div>
+</div>
+
             </div>
         </div>
 </section>
