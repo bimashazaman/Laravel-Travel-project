fb3c81a119295287888e8ee997ebb6c96854c059
bimashazaman <bimashazaman1234@gmail.com>
2022-07-07 03:56:02 +0600

    update form

--- resources/views/Backend/Admin/Tours/classicTours/UpdateClassicTour.blade.php
+++ resources/views/Backend/Admin/Tours/classicTours/UpdateClassicTour.blade.php
@@ -18,114 +18,160 @@
         </h2>
 
         <div class="row">
-            <div class="col-xl-6 d-flex">
+            <div class="col-xl-7 d-flex">
                 <div class="card flex-fill">
                     <div class="card-header">
                         <h4 class="card-title">Basic Information</h4>
                     </div>
-                    <div class="card-body">
-                        <form action="#">
+                    <div class="card-body" style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
+                        <form id="tourForm" action="" method="POST" enctype="multipart/form-data">
+                            {{csrf_field()}}
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">First Name</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
+                                <label class="col-form-label col-md-2">Tour name</label>
+                                <div class="col-md-10">
+                                    <input style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;" type="text" class="form-control" name="name">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Last Name</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
+                                <label class="col-form-label col-md-2">Description</label>
+                                <div class="col-md-10">
+                                    <textarea style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px; width:100%; border:none" name="description" cols="30" rows="10"></textarea>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Email Address</label>
-                                <div class="col-lg-9">
-                                    <input type="email" class="form-control">
+                                <label class="col-form-label col-md-2">Duration</label>
+                                <div class="col-md-10">
+                                    <input style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;" type="text" class="form-control" name="duration">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Username</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
+                                <label class="col-form-label col-md-2">Price</label>
+                                <div class="col-md-10">
+                                    <input style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;" type="number" class="form-control" name="price">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Password</label>
-                                <div class="col-lg-9">
-                                    <input type="password" class="form-control">
+                                <label class="col-form-label col-md-2">Start Date</label>
+                                <div class="col-md-10">
+                                    <input style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;" type="date" class="form-control" name="start_date">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Repeat Password</label>
-                                <div class="col-lg-9">
-                                    <input type="password" class="form-control">
+                                <label class="col-form-label col-md-2">End Date</label>
+                                <div class="col-md-10">
+                                    <input style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;" type="date" class="form-control" name="end_date">
                                 </div>
                             </div>
-                            <div class="text-end">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+        
+                            <!-- category dropdown -->
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-2">Default Select</label>
+                                <div class="col-md-10">
+                                    <select class="form-control form-select" name="category_id">
+                                        <option>-- Select --</option>
+                                        {{-- @foreach($categories as $category) --}}
+                                        <option value="">category name</option>
+                                        {{-- @endforeach --}}
+                                    </select>
+                                </div>
+                            </div>
+        
+                            <!-- destination dropdown -->
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-2">Default Select</label>
+                                <div class="col-md-10">
+                                    <select class="form-control form-select" name="destination_id">
+                                        <option>-- Select --</option>
+                                        {{-- @foreach($destinations as $destination) --}}
+                                        <option value="">destination name</option>
+                                        {{-- @endforeach --}}
+                                    </select>
+                                </div>
+                            </div>
+        
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-2">File Input </label>
+                                <div class="col-md-10">
+                                    <input style="box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;" class="form-control" type="file" name="images" multiple>
+                                </div>
+                            </div>
+                            <div style="float: right">
+                                <button class="btn btn-info" style="box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px; color:white; width:120px" type="submit">Create</button>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 d-flex">
+            <div class="col-xl-5">
+                <div class="card flex-fill">
+                    <div class="card-header">
+                        <h4 class="card-title">Programs</h4>
+                    </div>
+                    <div class="card-body" style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
+                        <form action="#">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Name</label>
+                                <div class="col-lg-9">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            
+                            <div class="text-end">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                            
+                        </form>
+                    </div>
+                </div>
                 <div class="card flex-fill">
                     <div class="card-header">
                         <h4 class="card-title">Facility</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body" style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
                         <form action="#">
                             <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Address 1</label>
+                                <label class="col-lg-3 col-form-label">Name</label>
                                 <div class="col-lg-9">
                                     <input type="text" class="form-control">
                                 </div>
                             </div>
-                            <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Address 2</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">City</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">State</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Country</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-lg-3 col-form-label">Postal Code</label>
-                                <div class="col-lg-9">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+                            
                             <div class="text-end">
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </div>
+                            
+                        </form>
+                    </div>
+                </div>
+                <div class="card flex-fill">
+                    <div class="card-header">
+                        <h4 class="card-title">Highlights</h4>
+                    </div>
+                    <div class="card-body" style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
+                        <form action="#">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Name</label>
+                                <div class="col-lg-9">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            
+                            <div class="text-end">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                            
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <h4 class="card-title">Highlights</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body" style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
                        
                         <form action="#">
                             <div class="row">
@@ -268,7 +314,7 @@
                     <div class="card-header">
                         <h4 class="card-title">Programs</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body" style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
                         <form action="#">
                             <div class="row">
                                 <div class="col-xl-6">
@@ -409,7 +455,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
     <!-- <script src="url('js/tour.js')"></script> -->
 @endsection
