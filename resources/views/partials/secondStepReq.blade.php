c4c0fb548751e12400bdb538456082b603195476
bimashazaman <bimashazaman1234@gmail.com>
2022-07-12 09:08:19 +0600

    All initial pages

--- resources/views/partials/secondStepReq.blade.php
+++ resources/views/partials/secondStepReq.blade.php
@@ -20,6 +20,32 @@
         <div class="row">
             <div class="col-md-9 col-xs-9">
                 <div class="mapouter"><div class="gmap_canvas"><iframe width="796" height="420" id="gmap_canvas" src="https://maps.google.com/maps?q=&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br><style>.mapouter{position:relative;text-align:right;height:420px;width:796px;}</style><a href="https://www.embedgooglemap.net">google maps html code</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:420px;width:796px;}</style></div></div>
+                <div class="row" style="margin: 30px; font-weight:600">
+                    <div class="col-md-3">Dhaka</div>
+                    <div class="col-md-3">Dhaka</div>
+                    <div class="col-md-3">Dhaka</div>
+                    <div class="col-md-3">Dhaka</div>
+                    
+                </div>
+                <div class="row" style="margin: 30px; font-weight:600">
+                    
+                    <div class="col-md-3">Dhaka</div>
+                    <div class="col-md-3">Dhaka</div>
+                    <div class="col-md-3">Dhaka</div>
+                    <div class="col-md-3">Dhaka</div>
+                </div>
+                <div class="row">
+                    <center style="font-weight:600">
+                        You can add one of your Tour here  <span style="color:Blue"> Activities</span>
+                    </center>
+                </div>
+                <div class="row" style="margin-top: 40px; margin-bottom:30px"> 
+                    <center>
+                        <button class="package-view">
+                            Send a qoute
+                        </button>
+                    </center>
+                </div>
             </div>
             <div class="col-md-3 col-xs-3 formReqSec">
                 <div class="row" style="margin-bottom: 10px">
@@ -38,6 +64,23 @@
                         12/12/2019
                     </span>
                 </div>
+                <div class="row" style="margin-bottom: 10px">
+                    <span style="margin-right: 10px; font-size:1.7rem">
+                        <i class="fa-solid fa-car"></i>
+                    </span>
+                    <span style="color: black">
+                        By Car
+                    </span>
+                </div>
+                <div class="row" style="margin-bottom: 10px">
+                    <span style="margin-right: 10px; font-size:1.7rem">
+                        <i class="fa-solid fa-location-dot"></i>
+                    </span>
+                    <span style="color: black">
+                        Dhaka, Bangladesh
+                    </span>
+                </div>
+
             </div>
         </div>
     </div>
